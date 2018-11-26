3dc82c86f84f6b1c8592f2556eb91495ba9f0e0d
Erwin Ried <1091420+eried@users.noreply.github.com>
2018-11-26 10:59:18 +0100

    Test

---
+++ generator/test/generate-all.bat
@@ -0,0 +1,4 @@
+generate base2.pptx input\stale.jpg input\stale.txt input\music.mp3 output\stale.mp4
+generate base2.pptx input\stale2.jpg input\stale2.txt input\music.mp3 output\stale2.mp4
+echo All done!
+pause 
---
+++ generator/test/generate.bat
@@ -0,0 +1,31 @@
+@echo off
+rem GOTO test
+IF "%5"=="" GOTO stop
+IF "%6"=="" GOTO continue
+
+rem We have all the params
+:continue
+copy %2 base2.support.jpg /y
+ReplaceText %1 temp.pptx %3
+RenderToVideo temp.pptx temp.mp4
+
+:test
+rem IF EXIST env (goto environment)
+rem python -m venv env
+rem pip install -r requirements.txt
+
+:environment
+env\Scripts\python.exe compose.py temp.mp4 %4 %5
+del temp.pptx
+del temp.mp4
+rem exit
+goto end
+
+rem Show help
+:stop
+echo.
+echo Usage: 
+rem                %1                   %2                %3                  %4              %5
+echo %0% input_presentation.pptx support_image.jpg replacement_text.txt input_music.mp3 output_video.mp4
+
+:end
